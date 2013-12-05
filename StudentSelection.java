89d16ba1db07778140eb9bad051c70a67bec3c9a
apcs <apcs@mvhs-u528ap15.dcsdk12.org>
2013-12-05 09:21:18 -0700

    Added group by Size or Number methods

--- StudentSelection.java
+++ StudentSelection.java
@@ -21,39 +21,92 @@ public class StudentSelection {
 		String input = reader.nextLine();
 
 		if (input.equals("Pick Random Student") || input.equals("1")) {
-			Random random = new Random();
-			int n = random.nextInt(students.length);
-			System.out.println(students[n]);
+			randomStudent(students);
+
 		} else if (input.equals("Group Students") || input.equals("2")) {
-			System.out.println("Group Size?");
-			int groupSize = reader.nextInt();
-			group(students, groupSize);
+			System.out.println("Group by Size of Groups(1) or Group by Number of Groups(2)");
+			input = reader.nextLine();
+
+			if (input.equals("Group by Size of Groups") || input.equals("1")) {
+				System.out.println("Group Size?");
+				int input2 = reader.nextInt();
+				printGroups(groupStudentsBySize(students, input2));
+			} else if (input.equals("Group by Number of Groups") || input.equals("2")) {
+				System.out.println("Number of Groups?");
+				int input2 = reader.nextInt();
+				printGroups(groupStudentsByNumber(students, input2));
+			}	
 		}
 	}
 
-	public static void group(String[] students, int groupSize) {
+	public static void randomStudent(String[] students) {
+		Random random = new Random();
+		int n = random.nextInt(students.length);
+		System.out.println(students[n]);
+	}
+
+	public static String[][] groupStudentsBySize(String[] students, int groupSize) {
+		students = mixStudents(students);
+
+		String[][] studentGroups = new String[students.length/groupSize+1][groupSize];
+
+		int count = 0;
+		for (int i=0; i<=students.length/groupSize+1; i++) {
+			//System.out.print("Group " + i +": ");
+			for (int j=0; j<groupSize; j++) {
+				if (count<students.length) {
+					studentGroups[i][j] = students[count];
+					//System.out.print(students[count] + "  ");
+					count++;
+				}
+			}
+			//System.out.println();
+			//System.out.println();
+		}
+		return studentGroups;
+		
+	}
+
+	public static String[][] groupStudentsByNumber(String[] students, int groupAmount) {
+		students = mixStudents(students);
+
+		String[][] studentGroups = new String[groupAmount][students.length/groupAmount];
+
+		int count = 0;
+		for (int i=0; i<groupAmount; i++) {
+			for (int j=0; j<students.length/groupAmount; j++) {
+				if (count<students.length) {
+					studentGroups[i][j] = students[count];
+					count++;
+				}
+			}
+		}
+		return studentGroups;
+	}
+
+	public static String[] mixStudents(String[] students) {
 		Random random = new Random();
 		for (int i=0; i<students.length; i++) {
 			int n = random.nextInt(31-i) + i;
-
 			String extraStudent = students[i];
 			students[i] = students[n];
 			students[n] = extraStudent;
 		}
+		return students;
+	}
 
-		int count = 0;
-
-		for (int i=1; i<=students.length/groupSize+1; i++) {
-			System.out.print("Group " + i +": ");
-			for (int j=0; j<groupSize; j++) {
-				if (count<students.length) {
-					System.out.print(students[count] + "  ");
-					count++;
-				}
+	public static void printGroups(String[][] studentGroups) {
+		for (int i=0; i<studentGroups.length; i++) {
+			System.out.print("Group " + (i+1) + ": ");
+			for (int j=0; j<studentGroups[i].length; j++) {
+				System.out.print(studentGroups[i][j] + "  ");
 			}
+
 			System.out.println();
 			System.out.println();
 		}
-		
-	}	
+	}
+
+
+
 }
